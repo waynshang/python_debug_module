4bd7023914c240e22145c7dec25c744689f20a80
wayneshang <wayne18308@gmail.com>
2022-02-15 21:19:31 +0800

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifier = [
+  'Development Status :: Production',
+  'Intended Audience :: Developers',
+  'License :: OSI Approved :: MIT License',
+  'programming Language :: Python :: 3.8'
+]
+
+setup(
+  name='debuggerModule',
+  version='0.01',
+  description='A package for create debug modules',
+  long_description=open('README.txt').read(),
+  author='Wayne Shang',
+  author_email='wayne18308@gmail.com',
+  license='MIT',
+  classifier=classifier,
+  keywords='debug',
+  packages=find_packages(),
+  install_requires=['']
+
+)
