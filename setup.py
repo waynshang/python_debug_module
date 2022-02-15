c03bb6d15dbe7302e007632413dfeb49cac909f1
wayneshang <wayne18308@gmail.com>
2022-02-15 22:52:19 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
 
 classifier = [
   'Development Status :: Production',
@@ -7,11 +14,23 @@ classifier = [
   'programming Language :: Python :: 3.8'
 ]
 
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+
 setup(
   name='debuggerModule',
-  version='0.3',
+  version='0.4',
   description='A package for create debug modules',
-  long_description=open('README.md').read(),
+  long_description="%s\n%s" % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.txt'))
+    ),
+  long_description_content_type="text/markdown",
   author='Wayne Shang',
   author_email='wayne18308@gmail.com',
   license='MIT',
